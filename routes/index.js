fd1368387e5c480cbcee27d2ac65460becbfdc1f
000raihan <000raihanahmad@gmail.com>
2023-05-03 10:40:28 +0600

    updated

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 
-// const base_url = "http://localhost:6002"
-const base_url = "http://116.68.200.97:56002"
+const base_url = "http://localhost:6002"
+// const base_url = "http://116.68.200.97:56002"
 
 const magazine_list = [
   {
@@ -14,7 +14,7 @@ const magazine_list = [
     sub_title: "February - April 2022"
   },
   {
-    id:3,
+    id:2,
     url:`${base_url}/loan`,
     thumb:"images/2022_2/CHRONICAL-12-01.jpg",
     title: "Loan Against Provident Fund",
@@ -28,10 +28,45 @@ const magazine_list = [
     sub_title: "September - November 2022"
   },
   {
-    id:3,
+    id:4,
     url:`${base_url}/travel_policy`,
     thumb:"images/2022_3/Chornical-13_page-0001.jpg",
     title: "Travel Policy",
+    sub_title: "September - November 2022"
+  },
+  {
+    id:5,
+    url:`${base_url}/retirement_policy`,
+    thumb:"images/2022_3/Chornical-13_page-0001.jpg",
+    title: "Retirement Policy",
+    sub_title: "September - November 2022"
+  },
+  {
+    id:6,
+    url:`${base_url}/radiant_csr_policy`,
+    thumb:"images/2022_3/Chornical-13_page-0001.jpg",
+    title: "Radiant_CSR Policy",
+    sub_title: "September - November 2022"
+  },
+  {
+    id:7,
+    url:`${base_url}/promotion_policy`,
+    thumb:"images/2022_3/Chornical-13_page-0001.jpg",
+    title: "Promotion Policy",
+    sub_title: "September - November 2022"
+  },
+  {
+    id:8,
+    url:`${base_url}/chronical-12`,
+    thumb:"images/2022_3/Chornical-13_page-0001.jpg",
+    title: "CHRONICAL-12",
+    sub_title: "September - November 2022"
+  },
+  {
+    id:9,
+    url:`${base_url}/financial_authority`,
+    thumb:"images/2022_3/Chornical-13_page-0001.jpg",
+    title: "Administrative and Financial Authority",
     sub_title: "September - November 2022"
   },
 ]
@@ -50,6 +85,22 @@ router.get('/posting_policy', function (req, res, next) {
 });
 router.get('/travel_policy', function (req, res, next) {
   res.render('travel_policy');
+});
+
+router.get('/retirement_policy', function (req, res, next) {
+  res.render('retirement_policy');
+});
+router.get('/radiant_csr_policy', function (req, res, next) {
+  res.render('radiant_csr_policy');
+});
+router.get('/promotion_policy', function (req, res, next) {
+  res.render('promotion_policy');
+});
+router.get('/chronical-12', function (req, res, next) {
+  res.render('chronical-12');
+});
+router.get('/financial_authority', function (req, res, next) {
+  res.render('financial_authority');
 });
 
 
